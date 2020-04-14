bca9c8442f56d85963d64052965948af1945439b
techtok7 <techtok7@gmail.com>
2020-04-14 17:13:55 +0530

    first commit

---
+++ patterns/001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    /* 
+        Pattern No. 1
+        input   : 7
+        output  : 1 2 3 4 5 6 7
+        Start Code From Here
+    */
+
+    for(int i=1; i<=n; i++)
+    {
+        printf("%d ",i);
+    }
+
+    printf("\n");
+    return 0;
+}
