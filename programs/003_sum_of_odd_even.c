1b7ad1fac13470d9912fe03fef02d70831f34ad7
techtok7 <techtok7@gmail.com>
2020-04-16 15:53:28 +0530

    first commit

--- programs/003_sum_of_odd_even.c
+++ programs/003_sum_of_odd_even.c
@@ -7,7 +7,7 @@
     1 to N.
 
     Input   : 10
-    Output  : Odd : 25
+    Output  : Odd  : 25
             : Even : 30
 
     Exaplanation : 
@@ -16,19 +16,12 @@
         1 3 5 7 9 = 25
         even
         2 4 6 8 10 = 30
-        total = 25+30 = 55
 
-    Solution 1 :
+    Solution    :
         Loop i to N and Check...
         if odd then add to odd
         else add to even
-    
-    Solution 2 :
-        Using Formula
-        ns = n*((n+1)/2);
 
-        Diffrence Between odd and Even
-        even-odd = n/2
 */
 #include <stdio.h>
 int main()
@@ -36,30 +29,18 @@ int main()
     int n;
     scanf("%d",&n);
 
-    int odd = 0;
-    int even = 0;
-
-    // First Way
+    int odd=0;
+    int even=0;
     for(int i=1; i<=n; i++)
     {
         if(i%2==1)
-        {
-            odd = odd+i;
-        }
+            odd = odd + i;
         else
-        {
-            even = even+i;
-        }
+            even = even + i;
     }
 
-    // Second Way
-    // int total =  n*((float)(n+1)/2);
-    // int diff = (n/2+(n%2));
-    // int odd = (total - diff)/2;
-    // int even = ((total - diff)/2)+diff;
-
-    printf("Odd  : %d \n",odd);
-    printf("Even : %d \n",even);
+    printf("Odd  : %d\n",odd);
+    printf("Even  : %d\n",even);
 
     printf("\n");
     return 0;
